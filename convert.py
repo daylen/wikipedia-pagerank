32106c0c38b662d6431554dd2456e993dfabf557
Daylen Yang <services@daylenyang.com>
2015-10-19 01:09:17 -0700

    First attempt at SQL file converter

---
+++ convert.py
@@ -0,0 +1,82 @@
+# coding=utf8
+
+"""
+Convert Wikipedia SQL dumps into more usable format.
+"""
+
+page_dump_filename = 'enwiki-20151002-page.sql'
+pagelinks_dump_filename = 'enwiki-20151002-pagelinks.sql'
+
+converted_page_dump_filename = 'title_id_dict.txt'
+
+def convert_page_dump():
+	title_id_dict = {}
+
+	print 'Reading page dump...'
+
+	with open(page_dump_filename) as f:
+		for line in f:
+			if not line.startswith("INSERT INTO"):
+				continue
+			line = line[line.index("(") + 1:]
+			pages = line.split("),(")
+			for page_row in pages:
+				page_metadata = page_row.split(",")
+				if page_metadata[1] == "0": # 0 is the article namespace
+					# Title -> ID
+					title_id_dict[page_metadata[2]] = int(page_metadata[0])
+			print len(title_id_dict.keys()), 'pages'
+
+	print 'Writing to file...'
+
+	converted_file = open('./' + converted_page_dump_filename, 'w+')
+	for key in title_id_dict:
+		converted_file.write(key + ' ' + str(title_id_dict[key]) + '\n')
+	converted_file.close()
+
+	return title_id_dict
+
+def restore_converted_page_dump():
+	title_id_dict = {}
+
+	print 'Restoring converted page dump...'
+
+	with open(converted_page_dump_filename) as f:
+		lines = f.readlines()
+		for line in lines:
+			arr = line.split(' ')
+			title_id_dict[arr[0]] = int(arr[1])
+
+	print 'Done'
+	
+	return title_id_dict
+
+title_id_dict = restore_converted_page_dump()
+
+pagelinks_list = []
+
+print 'Reading page links dump...'
+
+with open(pagelinks_dump_filename) as f:
+	for line in f:
+		if not line.startswith("INSERT INTO"):
+			continue
+		line = line[line.index("(") + 1:]
+		pagelinks = line.split("),(")
+		for pagelink_row in pagelinks:
+			pagelink_metadata = pagelink_row.split(",")
+			if pagelink_metadata[1] == "0": # 0 is the article namespace
+				from_id = int(pagelink_metadata[0])
+				try:
+					to_id = title_id_dict[pagelink_metadata[2]]
+					pagelinks_list.append((from_id, to_id))
+				except KeyError:
+					pass
+		print len(pagelinks_list), 'links'
+
+print 'Writing to file...'
+
+converted_file = open('./pagelinks_list.txt', 'w+')
+print >> converted_file, pagelinks_list
+converted_file.close()
+
