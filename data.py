764f7a96f8a9b824e9c9bf4b8ed6ae34570e9997
Brian Chu <bc@brianchu.com>
2015-10-21 08:06:50 -0700

    Added instructions

--- data.py
+++ data.py
@@ -27,19 +27,19 @@ with open('newid2title.txt', 'w') as f:
 del keys
 del values
 
-# with open('id2newid.txt', 'w') as f:
-#   for id, newid in id2newid.items():
-#     f.write('%i %i\n' % (id, newid))
+with open('id2newid.txt', 'w') as f:
+  for id, newid in id2newid.items():
+    f.write('%i %i\n' % (id, newid))
 
-# # @jit
-# def update_links_ids(id2newid):
-#   src = h5py.File('pagelinks_list.h5', 'r')
-#   lst = src['pagelinks_list'][()]
-#   src.close()
-#   for i in range(len(lst)):
-#     lst[i,0] = id2newid[lst[i,0]]
-#     lst[i,1] = id2newid[lst[i,1]]
-#   dst = h5py.File('newpagelinks_list.h5', 'w')
-#   dst.create_dataset('pagelinks_list', data=lst, compression='gzip')
-#   dst.close()
-# update_links_ids(id2newid)
+# @jit
+def update_links_ids(id2newid):
+  src = h5py.File('pagelinks_list.h5', 'r')
+  lst = src['pagelinks_list'][()]
+  src.close()
+  for i in range(len(lst)):
+    lst[i,0] = id2newid[lst[i,0]]
+    lst[i,1] = id2newid[lst[i,1]]
+  dst = h5py.File('newpagelinks_list.h5', 'w')
+  dst.create_dataset('pagelinks_list', data=lst, compression='gzip')
+  dst.close()
+update_links_ids(id2newid)
