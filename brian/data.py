53abb19c04e16a130e19b4a6906e9e147c39d01d
Brian Chu <bc@brianchu.com>
2015-10-22 08:22:14 -0700

    Update

--- brian/data.py
+++ brian/data.py
@@ -1,6 +1,8 @@
 import numpy as np
 import h5py
 import pickle
+import csv
+from collections import defaultdict
 
 num_ids = 12000926
 
@@ -9,7 +11,8 @@ with open('../title_id_dict.txt','r') as f:
   for line in f:
     c = line.split(' ')
     id = int(c[1].strip())
-    title = c[0].strip()
+    # strip the leading and trailing quotes
+    title = c[0].strip()[1:-1]
     kvs.append((id, title))
 kvs.sort(key=lambda pair: pair[0])
 keys, values = list(zip(*kvs))
@@ -22,27 +25,72 @@ for key in keys:
   count += 1
 
 newid2title = {}
+title2newid = {}
 for key, value in zip(range(len(values)), values):
   newid2title[key] = value
+  title2newid[value] = key
 del keys
 del values
 
 with open('newid2title.pickle', 'wb') as f:
   pickle.dump(newid2title, f, protocol=pickle.HIGHEST_PROTOCOL)
 
+with open('title2newid.pickle', 'wb') as f:
+  pickle.dump(title2newid, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open('newid2title.txt', 'w') as f:
+  for key, value in newid2title.items():
+    f.write('%i %s\n' % (key, value))
+
+with open('id2newid.pickle', 'wb') as f:
+  pickle.dump(id2newid, f, protocol=pickle.HIGHEST_PROTOCOL)
+
 with open('id2newid.txt', 'w') as f:
   for id, newid in id2newid.items():
     f.write('%i %i\n' % (id, newid))
 
-num_links = 374556579
-def update_links_ids(id2newid):
-  with open('../pagelinks_list.txt', 'r') as f:
-    lst = np.empty((num_links, 2), dtype=np.int32)
-    for i, line in enumerate(f):
-      c = line.split(' ')
-      lst[i,0] = id2newid[np.int(c[0])]
-      lst[i,1] = id2newid[np.int(c[1])]
-    dst = h5py.File('newpagelinks_list.h5', 'w')
-    dst.create_dataset('pagelinks_list', data=lst, compression='gzip')
-    dst.close()
-update_links_ids(id2newid)
+#### Category links data
+
+category2ids = defaultdict(set)
+id2categories = defaultdict(set)
+
+with open('categorylinks.csv', newline='', errors='ignore') as f:
+  links = csv.reader(f)
+  for link in links:
+    try:
+      id = int(link[0])
+      id = id2newid[id]
+    except (ValueError, KeyError):
+      continue
+    category = link[1]
+    category2ids[category].add(id)
+    id2categories[id].add(category)
+  del links
+
+with open('category2ids.pickle', 'wb') as f:
+  pickle.dump(category2ids, f, protocol=pickle.HIGHEST_PROTOCOL)
+del category2ids
+
+id2categories1 = {key:value for i,(key, value) in enumerate(id2categories.items()) if i % 2 == 0}
+with open('id2categories1.pickle', 'wb') as f:
+  pickle.dump(id2categories1, f, protocol=pickle.HIGHEST_PROTOCOL)
+del id2categories1
+
+id2categories2 = {key:value for i,(key, value) in enumerate(id2categories.items()) if i % 2 == 1}
+with open('id2categories2.pickle', 'wb') as f:
+  pickle.dump(id2categories2, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+#### Links data
+
+# num_links = 374556579
+# def update_links_ids(id2newid):
+#   with open('../pagelinks_list.txt', 'r') as f:
+#     lst = np.empty((num_links, 2), dtype=np.int32)
+#     for i, line in enumerate(f):
+#       c = line.split(' ')
+#       lst[i,0] = id2newid[np.int(c[0])]
+#       lst[i,1] = id2newid[np.int(c[1])]
+#     dst = h5py.File('newpagelinks_list.h5', 'w')
+#     dst.create_dataset('pagelinks_list', data=lst, compression='gzip')
+#     dst.close()
+# update_links_ids(id2newid)
--- brian/pagerank.py
+++ brian/pagerank.py
@@ -1,10 +1,10 @@
 import numpy as np
 import h5py
+import pickle
 from scipy.io import savemat
 from scipy.sparse import csr_matrix, csc_matrix, lil_matrix, coo_matrix
-from signal import signal, SIGINT, SIG_DFL
 from sklearn.preprocessing import normalize
-import pickle
+from subprocess import check_output
 
 src = h5py.File('newpagelinks_list.h5', 'r')
 links = src['pagelinks_list']
@@ -20,13 +20,42 @@ M = M.tocsr()
 normalize(M, norm='l1', copy=False)
 # we will find right eigenvalue, so convert matrix to form matrix[to_idx][from_idx], transpose implicitly gives csc_matrix, so covert back to csr
 M = M.transpose().tocsr()
+del links
+del src
+
+print("Set up matrix")
 
 with open('newid2title.pickle', 'rb') as f:
-  id2title = pickle.load(f)
+   id2title = pickle.load(f)
 
-teleport = 0.2
-def run():
-  random_arrival = teleport * np.ones(n, dtype=np.float64) / n
+with open('title2newid.pickle', 'rb') as f:
+   title2id = pickle.load(f)
+
+with open('id2newid.pickle', 'rb') as f:
+  id2newid = pickle.load(f)
+
+# with open('id2categories1.pickle', 'rb') as f:
+  # id2categories1 = pickle.load(f)
+
+# with open('id2categories2.pickle', 'rb') as f:
+  # id2categories2 = pickle.load(f)
+
+with open('category2ids.pickle', 'rb') as f:
+  category2ids = pickle.load(f)
+
+print("Loaded dicts")
+
+teleport = 0.15
+def run(arrival_indices=None):
+  # topic weighting
+  if arrival_indices:
+    random_arrival = np.zeros(n, dtype=np.float64)
+    for idx in arrival_indices:
+      random_arrival[idx] = 1
+    random_arrival *= teleport / len(arrival_indices)
+  # no topic weighting
+  else:
+    random_arrival = teleport * np.ones(n, dtype=np.float64) / n
 
   def iterate(v):
     newv = M.dot(v)
@@ -37,23 +66,109 @@ def run():
   v = np.ones(n) / n
   oldv = np.zeros(n)
 
-  sigint_called = False
-  def sigint_handler(num, frame):
-      global sigint_called
-      sigint_called = True
-  signal(SIGINT, sigint_handler)
-
   iteration = 0
-  while abs(np.sum(v - oldv)) > 0.00000000001 and not sigint_called:
+  while abs(np.sum(v - oldv)) > 0.000000001:
     oldv = v
     v = iterate(v)
     iteration += 1
-    print('Finished iteration: %i' % iteration)
-
-  signal(SIGINT, SIG_DFL)
+    if iteration % 10 == 0:
+      print('Finished iteration:', iteration)
+  print('Total iterations:', iteration)
   return v
 
-v = run()
-top = v.argsort()[::-1][:20]
-for top_idx in top:
-  print('Idx: %i, with title: %s' % (top_idx, id2title[top_idx]) )
+def print_top(v):
+  top = v.argsort()[::-1][:20]
+  for top_idx in top:
+    print('Idx: %i, with title: %s' % (top_idx, id2title[top_idx]) )
+
+school_experiments = [
+  'science',
+  'engineering',
+  'math',
+  'artist',
+  'music',
+  'literature',
+]
+schools = [
+  'University_of_California,_Berkeley',
+  'Stanford_University',
+  'Harvard_University',
+  'California_Institute_of_Technology',
+  'Massachusetts_Institute_of_Technology',
+  'Princeton_University',
+  'Yale_University',
+  'Columbia_University',
+  'Brown_University',
+  'University_of_Oxford',
+  'University_of_Cambridge',
+  'Carnegie_Mellon_University',
+  'University_of_Pennsylvania',
+  'University_of_Chicago',
+  'Duke_University',
+  'Cornell_University',
+  'New_York_University',
+  'Juilliard_School',
+  'Berklee_College_of_Music',
+  'Rhode_Island_School_of_Design'
+]
+
+school_indices = [title2id[title] for school in schools]
+school_experiment_ids = []
+for experiment in school_experiments:
+  categories = check_output(['grep', '-i', experiment, 'categories.txt']).decode('utf-8')
+  categories = categories.split()
+  ids = set()
+  for category in categories:
+    ids = ids.union(category2ids[category])
+  school_experiment_ids.append(ids)
+
+for experiment, ids in zip(school_experiments, school_experiment_ids):
+  print('\n****Running:', experiment)
+  v = run(ids)
+  print_top(v)
+  scores = v[school_indices]
+  # indices in school_titles array of top ranking schools to low ranking schools
+  order = scores.argsort()[::-1]
+  for idx in order:
+    print('%s has %f' % (schools[idx], scores[idx]) )
+
+candidate_experiments = [
+  'immigration',
+  'gun_control',
+  'welfare',
+  'abortion',
+  'campaign_finance',
+  'healthcare'
+]
+candidates = [
+  'Bernie_Sanders',
+  'Hillary_Clinton',
+  'Donald_Trump',
+  'Jeb_Bush',
+  'Ben_Carson',
+  'Carly_Fiorina',
+  'Joe_Biden',
+  'Marco_Rubio',
+  'Chris_Christie',
+  'Lawrence_Lessig',
+]
+candidate_indices = [title2id[candidate] for candidate in candidates]
+candidate_experiment_ids = []
+for experiment in candidate_experiments:
+  categories = check_output(['grep', '-i', experiment, 'categories.txt']).decode('utf-8')
+  categories = categories.split()
+  ids = set()
+  for category in categories:
+    ids = ids.union(category2ids[category])
+  candidate_experiment_ids.append(ids)
+
+for experiment, ids in zip(candidate_experiments, candidate_experiment_ids):
+  print('\n****Running:', experiment)
+  v = run(ids)
+  print_top(v)
+  scores = v[candidate_indices]
+  # indices in candidate_titles array of top ranking candidates to low ranking candidates
+  order = scores.argsort()[::-1]
+  for idx in order:
+    print('%s has %f' % (candidates[idx], scores[idx]) )
+
